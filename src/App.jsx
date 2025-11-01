7248aa55c63d79cec8ff0638b6f9a178a6ed3529
aiden <rlaqhtjs789@naver.com>
2025-11-01 17:42:03 +0900

    todo list 가 데이터 연동(기능 x)

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,28 @@ import { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  todoData = [
+    { id: 1, title: "공부하기", completed: false },
+    { id: 2, title: "청소하기", completed: false },
+  ];
 
   btnStyle = {
-    color: '#fff',
-    border: 'none',
-    padding: '5px 9px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right',
-    backgroundColor: '#ff0000',
-  }
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+    backgroundColor: "#ff0000",
+  };
 
   getStyle = () => {
     return {
-      padding: '10px',
-      borderBottom: '1px #ccc dotted',
-      textDecoration: 'none',
-    }
-  }
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
 
   render() {
     return (
@@ -28,18 +32,13 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-
-          <div style={this.getStyle()}>
-            <input type="checkbox" defaultChecked={false} />
-            공부하기
-            <button style={this.btnStyle}>X</button>
-          </div>
-
-          <div style={this.getStyle()}>
-            <input type="checkbox" defaultChecked={false} />
-            청소하기
-            <button style={this.btnStyle}>X</button>
-          </div>
+          {this.todoData.map((data) => (
+            <div key={data.id} style={this.getStyle()}>
+              <input type="checkbox" value={data.id} defaultChecked={data.completed} />
+              {data.title}
+              <button style={this.btnStyle}>X</button>
+            </div>
+          ))}
         </div>
       </div>
     );
