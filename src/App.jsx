f50dd602fa18260ce91b1b20ff3848046b07bd1a
aiden <rlaqhtjs789@naver.com>
2025-11-01 17:32:04 +0900

    list ui 퍼블리싱(기능 x)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { Component } from 'react'
-import './App.css'
+import { Component } from "react";
+import "./App.css";
 
 export default class App extends Component {
+
+  btnStyle = {
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right',
+    backgroundColor: '#ff0000',
+  }
+
+  getStyle = () => {
+    return {
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: 'none',
+    }
+  }
+
   render() {
     return (
-      <div className='container'>
-        <div className='todoBlock'>
-          <div className='title'>
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
             <h1>할 일 목록</h1>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button style={this.btnStyle}>X</button>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            청소하기
+            <button style={this.btnStyle}>X</button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
