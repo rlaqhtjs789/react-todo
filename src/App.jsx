c60a6d767a213f046c8a1135f06adb6c4d666788
aiden <rlaqhtjs789@naver.com>
2025-11-07 14:41:21 +0900

    [react todo] lists 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Component, useState } from "react";
 import "./App.css";
+import Lists from "./components/Lists";
 
 export default function App() {
   const [todoData, setTodoData] = useState([
@@ -7,28 +8,6 @@ export default function App() {
     { id: 2, title: "청소하기", completed: false },
   ]);
   const [value, setValue] = useState("");
-
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-    backgroundColor: "#ff0000",
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
-  const handleClick = (id) => {
-    setTodoData(todoData.filter((data) => data.id !== id));
-  };
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -46,41 +25,14 @@ export default function App() {
     setValue("");
   };
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-
-    setTodoData(newTodoData);
-  };
-
   return (
     <div className="container">
       <div className="todoBlock">
         <div className="title">
           <h1>할 일 목록</h1>
         </div>
-        {todoData.map((data) => (
-          <div key={data.id} style={getStyle(data.completed)}>
-            <input
-              type="checkbox"
-              checked={data.completed}
-              onChange={() => handleCompleteChange(data.id)}
-            />
-            {data.title}
-            <button
-              style={btnStyle}
-              onClick={() => {
-                handleClick(data.id);
-              }}
-            >
-              X
-            </button>
-          </div>
-        ))}
+
+        <Lists todoData={todoData} setTodoData={setTodoData} />
 
         <form style={{ display: "flex" }} onSubmit={handleSubmit}>
           <input
